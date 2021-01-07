1ea6c4572519eccc68689294bd51749815b151d7
xtreme6007 <prestonnichols96@gmail.com>
2021-01-07 11:41:45 -0600

    finsished method

--- src/academy/learnprograming/Main.java
+++ src/academy/learnprograming/Main.java
@@ -5,19 +5,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        inputThenPrintSumAndAverage();
 
     }
 
     public static void inputThenPrintSumAndAverage() {
         Scanner scanner = new Scanner(System.in);
+        int count = 0;
         int sum = 0;
-        int average = 0;
+        double average = 0;
         while (true) {
+            System.out.println("Please Enter Number");
             boolean isAnInt = scanner.hasNextInt();
+            if (isAnInt) {
+                int number = scanner.nextInt();
+                count++;
+                sum += number;
 
+            } else {
+                System.out.println("Invalid Number");
+                break;
+            }
 
 
         }
-
+        average = (double) Math.round(sum/count);
+        System.out.println("SUM = " + sum + " AVG = " + average);
+        scanner.close();
     }
 }
